d8e8b6c096c25db21e311deab27f7d956be779f1
dost6ryanb <dost6.ryan.bantillo@gmail.com>
2015-07-16 14:45:35 +0800

    +added missing lib/init.php

---
+++ lib/init.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'lib/devices.class.php';
+	date_default_timezone_set('Asia/Manila');
+
+	$now = date("H:i");
+	$hour = intval(substr($now, 0, 2));
+	$sdate;
+
+	if ($hour >= 0 && $hour < 8 ) {
+			$sdate = date("m/d/Y", strtotime("yesterday"));
+	} elseif ($hour >= 8 && $hour <= 23) {
+			$sdate = date("m/d/Y");
+	}
+?>
+<script type="text/javascript" >
+    var DOCUMENT_ROOT = "http://localhost/~DRRMU-VI/bantaypanahon/";
+    var SERVER_DATE = '<?php echo date("M d,Y");?>';
+    var SERVER_TIME = '<?php echo date("g:i A");?>';
+</script>
