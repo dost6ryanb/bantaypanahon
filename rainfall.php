496f7b91501a0db10baf76aff33eb21fe56e4ee4
dost6ryanb <dost6.ryan.bantillo@gmail.com>
2016-07-11 16:57:50 +0800

    Rainfall Monitoring Chart

--- rainfall.php
+++ rainfall.php
@@ -5,6 +5,7 @@
 	<script type="text/javascript" src='js/jquery-1.11.1.min.js'></script>
 	<script type="text/javascript" src='js/jquery-ui.min.js'></script>
 	<script type="text/javascript" src='js/date-en-US.js'></script>
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 	<link rel="stylesheet" href='css/jquery-ui.min.css'>
 	<link rel="stylesheet" href='css/jquery-ui.theme.min.css'>
 	<link rel="stylesheet" href='css/jquery-ui.structure.min.css'>
@@ -35,6 +36,7 @@
 		  };
 
 	var xtblcounter = 0;
+	google.charts.load('current', {packages: ['corechart']});
 	$(document).ready(function() {
 		$("button").button();
 
@@ -75,26 +77,29 @@
 
 		var table = $('<table/>', {'class':'xtbl', 'id':'xtbl_'+ ++xtblcounter, 'data-duration':duration}).prependTo(div);
 		//$('<tr><th>Server DateTime</th><td id="serverdtr">'+key['serverdate']+' '+ key['servertime']+'</td><tr>').appendTo(table);
-		$('<tr/>').append($('<th colspan="2">Cumulative Rainfall Reading of '+ province +' for the last '+ parseInt(duration/60) +' hour/s.</th><'))
-						.append($('<td/>@ ', {'class':'textalignright'}).text(key['serverdate']+' '+ key['servertime'])
-							.append($('<button>close</button>')
-								.on('click', function() {table.remove();})
+		$('<tr/>')
+		.append($('<th colspan="2" class="ui-widget-header">Cumulative Rainfall Reading of '+ province +' for the last '+ parseInt(duration/60) +' hour/s.</th>').append('<button id="cx-xtbl_'+ xtblcounter +'">close</button>'))
+						// .append($('<td/>@ ', {'class':'textalignright'}).text(key['serverdate']+' '+ key['servertime'])
+							// .append($('<button>close</button>')	
+					.appendTo(table);
+		
+		$("#cx-xtbl_"+ xtblcounter).on('click', function() {table.remove();})
 								.button({
 							      icons: {
 							        primary: "ui-icon-cancel"
 							      },
-							      text: true})
-					)).appendTo(table);
-
-		$('<tr class="ui-widget-header"><th>Municipality</th><th>Location</th><th>Cumulative (mm)</th><tr>').appendTo(table);
+							      text: true});
+		// $('<tr class="ui-widget-header"><th>Municipality</th><th>Location</th><th>Cumulative (mm)</th><tr>').appendTo(table);
+		//$('<tr class="ui-widget-header"><th>Location</th><th>Cumulative (mm)</th><tr>').appendTo(table);
+		//$('<tr class="ui-widget-header"><th colspan="2">Cumulative (mm)</th><tr>').appendTo(table);
 
 		for(var i=0;i<rainfall_devices.length;i++) {
 			var cur = rainfall_devices[i];
 			if (cur['province_name'] == province) {
 				$('<tr/>', {'data-dev_id':cur['dev_id']})
-				.append($('<td>'+cur['municipality_name']+'</td>'))
-				.append($('<td>'+cur['location_name']+'</td>'))
-				.append($('<td/>', {'data-col':'cr'})).appendTo(table);
+				.append($('<td style="width:80px">'+cur['municipality_name']+ " - " + cur['location_name'] + '</td>'))
+				// .append($('<td>'+cur['location_name']+'</td>'))
+				.append($('<td/>', {'colspan':'2','data-col':'cr'})).appendTo(table);
 				if (cur['status_id'] == null || cur['status_id'] == '0') {
 					postGetData('xtbl_'+xtblcounter, cur['dev_id'], "", "", duration);
 				} else {
@@ -134,8 +139,9 @@
 			) {
 			updateRainfallTable(xtbl, device_id, '[NO DATA]', 'nodata') ;
 		} else {			
-			var rain_cumulative = solveforcumulative(data, duration)
-			updateRainfallTable(xtbl, device_id, rain_cumulative) ;
+			var rain_cumulative = solveforcumulative(data, duration, xtbl, device_id);
+			//updateRainfallTable(xtbl, device_id, rain_cumulative) ;
+
 		}
 	}
 
@@ -165,6 +171,9 @@
 					break;
 				} 
 			}
+
+			
+
 		} else {
 			cr.text("");
 		}
@@ -176,17 +185,102 @@
 		}
 	}
 
-	function solveforcumulative(data, duration) {
+	function drawChartRain(xtbl, dev_id, json) {
+		var table = $(document.getElementById(xtbl));
+		var cr = table.find("tr[data-dev_id='" + dev_id + "'] td[data-col='cr']");
+		var div_id = xtbl + '-' + dev_id;
+		var c = $('<div id="'+ div_id + '"" class="rain-chart"></div>');
+		cr.append(c);
+		//console.log(c);
+		var chartdiv = xtbl + '-' + dev_id;
+	  	var datatable = new google.visualization.DataTable();
+		datatable.addColumn('datetime', 'DateTimeRead');
+		datatable.addColumn('number', 'Cumulative Rain');
+		datatable.addColumn('number', 'Rain Value');
+		
+		//j - index of data
+		// i - index of column
+		for(var j=0;j<json.data.length;j++) {
+			var row = Array(3);
+			//console.log(json.data[j].dateTimeRead);
+			row[0] = Date.parseExact(json.data[j].dateTimeRead, 'yyyy-MM-dd HH:mm:ss');
+			row[1] = {
+					v:parseFloat(json.data[j].rain_cumulative), //cumulative rain
+					f:json.data[j].rain_cumulative + ' mm'
+				}
+			row[2] = {
+					v:parseFloat(json.data[j].rain_value), //rain value
+					f:json.data[j].rain_value + ' mm'
+				}
+			
+			datatable.addRow(row);
+			
+		}
+		var maxdate;
+		var mindate;
+
+		var d =  Date.parseExact(json.data[json.data.length - 1].dateTimeRead, 'yyyy-MM-dd HH:mm:ss');
+		var d2 =  Date.parseExact(json.data[0].dateTimeRead, 'yyyy-MM-dd HH:mm:ss');
+
+		//var title_startdatetime = d.toString('MMMM d yyyy h:mm:ss tt'); //from last data
+		var title_startdatetime = d.toString('MMMM d yyyy h:mm:ss tt'); // from 8:00 AM
+		var title_enddatetime = d2.toString('MMMM d yyyy h:mm:ss tt');
+
+		var options = {
+		  title: 'Rainfall Reading from ' + title_startdatetime + ' to ' + title_enddatetime,
+		  hAxis: {
+		    title: 'Rainfall Cumulative: ' + json.data[0].rain_cumulative + " mm", 
+			format : 'LLL d h:mm:ss a',
+			viewWindow : {min : d, max : d2},
+			textStyle : {fontSize: 10}
+		  },
+		  vAxes : {
+		  	0 : {
+		  		title: 'Rain Value (mm)',
+				format: '# mm',
+				minValue: '0',
+				maxValue: '50'
+		  	},
+		  	1 : {
+		  		title: 'Cumulative (mm)',
+		  		direction: -1,
+		  		format: '# mm',
+		  		minValue: '0',
+				maxValue: '200',
+		  	}
+		  },
+		  pointsize: 3,
+		  seriesType: "line",
+          series: {
+          	0 : {
+          		type: "line",
+          		targetAxisIndex : 1
+          	},
+          	1: {
+          		type: "bars",
+          		targetAxisIndex : 0
+          		}
+          },
+		  crosshair : {trigger: 'both'}
+        };
+		var chart =  new google.visualization.ComboChart(document.getElementById(chartdiv));
+        chart.draw(datatable, options);
+	  }
+
+	function solveforcumulative(data, duration, xtbl, device_id) {
 		var serverdtr = Date.parseExact(key['serverdate']+ ' '+ key['servertime']+':00', 'MM/dd/yyyy HH:mm:ss');
 		var str = '';
-		var cr = 0;
+		var cr = null;
 		var enddtr = serverdtr.clone().addMinutes(-parseInt(duration));
 
-		for(var i=0;i<data.data.length;i++) {
+		var i=0;
+		for(i=0;i<data.data.length;i++) {
 			var devicedtr = Date.parseExact(data.data[i].dateTimeRead, 'yyyy-MM-dd HH:mm:ss');
 
 			if (devicedtr.between(enddtr, serverdtr)) {
+				if (cr == null) cr = 0;
 				cr += parseFloat(data.data[i].rain_value);
+				data.data[i].rain_cumulative = cr;
 			} else {
 				if (i==0) {
 					cr += parseFloat(data.data[i].rain_value);
@@ -194,12 +288,31 @@
 					enddtr = serverdtr.clone().addMinutes(-parseInt(duration));
 					str = ' <a class="ui-state-error" href="#" title="Out of Sync. Result from ' + serverdtr.toString('MMMM d yyyy h:mm:ss tt') + '">[!]</a>';
 				} else {
+					
 					break;
 				}
 				
 			}
 		}
 
+		var t = data;
+		t.data.splice(i);
+		t.count = i;
+
+		var rel_cr = 0.00;
+
+
+		for (i=t.data.length-1;i>=0;i--) {
+			//console.log(t.data[i].dateTimeRead + " - " + t.data[i].rain_value + " - " + rel_cr);
+			var devicedtr = Date.parseExact(t.data[i].dateTimeRead, 'yyyy-MM-dd HH:mm:ss');
+			rel_cr += parseFloat(t.data[i].rain_value);
+			t.data[i].rain_cumulative = rel_cr.toFixed(2);
+		}
+		//console.log(t);
+
+		
+		drawChartRain(xtbl, device_id, t);
+		
 		return cr.toFixed(2) + str;
 
 	}
