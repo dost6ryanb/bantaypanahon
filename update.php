b771a1e75be4a341c220e054f41c7c109b4e80b4
dost6ryanb <dost6.ryan.bantillo@gmail.com>
2016-09-08 12:28:46 +0800

    Fixed JSON responses int to string

--- update.php
+++ update.php
@@ -1,19 +1,19 @@
 <?php
 $dev_id = $_POST['dev_id'];
-$status_id = $_POST['status_id'];
+$status = $_POST['status'];
 $tryauth = $_POST['tryauth'];
 
 if (empty($tryauth) || $tryauth <> "orddrrmu6") return;
 
-if (empty($dev_id) || (empty($status_id) && $status_id <> 0)) return;
+if (empty($dev_id) || (empty($status) && $status <> 0)) return;
 
 include_once 'lib/devices.class.php';
-$result = Devices::updateStatusId($dev_id, $status_id);
+$result = Devices::updateStatusId($dev_id, $status);
 
 header('Content-Type: application/json');
 
 if ($result == true) {
-	echo '{"success":true, "dev_id":'.$dev_id.', "status_id":'.$status_id.'}';
+	echo '{"success":true, "dev_id":"'.$dev_id.'", "status":"'.$status.'"}';
 } else {
 	echo '{"success":false}';
 }
