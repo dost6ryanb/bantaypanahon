7c7a455a16bba4fdcfbc4a5eee4af42645e788ce
dost6ryanb <dost6ryanb@gmail.com>
2017-04-06 19:14:01 +0800

    move xhrPool to ajax global object

--- js/helper.js
+++ js/helper.js
@@ -1,22 +1,22 @@
 //helper.js
 var xhrPoolHelper = (function($) {
-    var xhrPool = [];
+    $.xhrPool = [];
     $(document).ajaxSend(function(e, jqXHR, options){
-        xhrPool.push(jqXHR);
+        $.xhrPool.push(jqXHR);
     });
     $(document).ajaxComplete(function(e, jqXHR, options) {
-        var index = xhrPool.indexOf(jqXHR);
+        var index = $.xhrPool.indexOf(jqXHR);
         if (index > -1) {
-            xhrPool.splice(index, 1);
+            $.xhrPool.splice(index, 1);
         }
     });
 
     return {
         abortAll: function() {
-            $.each(xhrPool, function(idx, jqXHR) {
+            $.each($.xhrPool, function(idx, jqXHR) {
                 jqXHR.abort();
             });
-            xhrPool.length = 0;
+            $.xhrPool = [];
         }
     }
 });
