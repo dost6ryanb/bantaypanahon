9466c53de1687430c078f4eb2593601b3851090a
dost6ryanb <dost6ryanb@gmail.com>
2017-06-08 10:51:58 +0800

    converted file_get_contents to cURL

--- data.php
+++ data.php
@@ -13,25 +13,33 @@ if ($edate == FALSE) $edate = '';
 $url = 'http://fmon.asti.dost.gov.ph/api/index.php/device/getData/';
 $data = array('start' => '0', 'limit' => $limit, 'sDate' => $sdate, 'eDate' => $edate, 'pattern' => $dev_id);
 
-$options = array(
-	'http' => array(
-		'header'  => "Connection: close\r\nContent-type: application/x-www-form-urlencoded\r\n",
-		'method'  => 'POST',
-		'content' => http_build_query($data)/*,
-		'proxy' => 'tcp://192.168.1.146:8888'*/
-	),
-);
+//$options = array(
+//	'http' => array(
+//		'header'  => "Connection: close\r\nContent-type: application/x-www-form-urlencoded\r\n",
+//		'method'  => 'POST',
+//		'content' => http_build_query($data)/*,
+//		'proxy' => 'tcp://192.168.1.146:8888'*/
+//	),
+//);
+//
+//$context  = stream_context_create($options);
+//$response = @file_get_contents($url, false, $context);
 
-//Cache-Control: public, max-age=60\r\n
-$context  = stream_context_create($options);
-$result = @file_get_contents($url, false, $context);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+// execute!
+$response = curl_exec($ch);
+
+// close the connection, release resources used
+curl_close($ch);
+
 header('Access-Control-Allow-Origin: *');
-
-if ($result == FALSE) {
+if ($response == FALSE) {
 	echo '{"device":[{"dev_id":'.$dev_id.'}],"count":-1}';
 } else {
-	echo $result;
+	echo $response;
 }
-
 
 ?>
