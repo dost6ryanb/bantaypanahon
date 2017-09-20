33424e917e130e8298450090afa01499f8cacba4
dost6ryanb <dost6.ryan.bantillo@gmail.com>
2017-09-20 18:17:05 +0800

    Added 'experimental' cache mechanism to minimize requesting data from ASTI api

--- data.php
+++ data.php
@@ -9,37 +9,67 @@ if ($limit == FALSE) $limit = '';
 if ($sdate == FALSE) $sdate = '';
 if ($edate == FALSE) $edate = '';
 
-//$url = 'http://fmon.asti.dost.gov.ph/weather/home/index.php/device/getData/';
-$url = 'http://fmon.asti.dost.gov.ph/api/index.php/device/getData/';
-$data = array('start' => '0', 'limit' => $limit, 'sDate' => $sdate, 'eDate' => $edate, 'pattern' => $dev_id);
-
-//$options = array(
-//	'http' => array(
-//		'header'  => "Connection: close\r\nContent-type: application/x-www-form-urlencoded\r\n",
-//		'method'  => 'POST',
-//		'content' => http_build_query($data)/*,
-//		'proxy' => 'tcp://192.168.1.146:8888'*/
-//	),
-//);
-//
-//$context  = stream_context_create($options);
-//$response = @file_get_contents($url, false, $context);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
-// execute!
-$response = curl_exec($ch);
-
-// close the connection, release resources used
-curl_close($ch);
-
+$key = md5("$dev_id-$limit-$sdate-$edate");
 header('Access-Control-Allow-Origin: *');
-if ($response == FALSE) {
-	echo '{"device":[{"dev_id":'.$dev_id.'}],"count":-1}';
-} else {
-	echo $response;
+header('Cache-Control: max-age=300, private');
+
+$response = getCacheAndPrint($key);
+
+if (!$response) {
+    $response = getDataFromPredictService($dev_id, $limit, $sdate, $edate);
+    if (!$response) {
+        echo '{"device":[{"dev_id":'.$dev_id.'}],"count":-1}';
+    } else {
+        echo $response;
+        putCache($key, $response);
+    }
 }
 
+function getDataFromPredictService($dev_id, $limit, $sdate, $edate) {
+    $url = 'http://fmon.asti.dost.gov.ph/api/index.php/device/getData/'; //ASTI API
+    $data = array('start' => '0', 'limit' => $limit, 'sDate' => $sdate, 'eDate' => $edate, 'pattern' => $dev_id);
+    $ch = curl_init($url);
+    //curl_setopt($ch, CURLOPT_PROXY, '192.168.1.100:8888');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+}
+
+function getCacheAndPrint($key) {
+    $cache_life = intval(8); // minutes
+    if ($cache_life <= 0) return null;
+
+    // fully-qualified filename
+    $fqfname = getCacheFileName($key);
+
+    if (file_exists($fqfname)) {
+        $filetime = filemtime($fqfname);
+        $expirytime = (time() - 60 * $cache_life);
+        if ($filetime > $expirytime) {
+            // The cache file is fresh.
+            //$fresh = file_get_contents($fqfname);
+            //$results = json_decode($fresh,true);
+            readfile($fqfname);
+            return true;// $fresh;
+        }
+        else {
+            unlink($fqfname);
+        }
+    }
+
+    return null;
+}
+
+function putCache($key, $results) {
+    //$json = json_encode($results);
+    $fqfname = getCacheFileName($key);
+    file_put_contents($fqfname, $results, LOCK_EX);
+}
+
+function getCacheFileName($key) {
+    return "cache/$key.json";
+}
 ?>
